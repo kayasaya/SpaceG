9f30645b769db73c0615dd2b82f4bb68f11f5ad4
kayasaya <xzerotekken@gmail.com>
2018-02-01 16:30:24 -0600

    1
    1

---
+++ js/main.js
@@ -0,0 +1,213 @@
+jQuery(document).ready(function($){
+	var formModal = $('.cd-user-modal'),
+		formLogin = formModal.find('#cd-login'),
+		formSignup = formModal.find('#cd-signup'),
+		formForgotPassword = formModal.find('#cd-reset-password'),
+		formModalTab = $('.cd-switcher'),
+		tabLogin = formModalTab.children('li').eq(0).children('a'),
+		tabSignup = formModalTab.children('li').eq(1).children('a'),
+		forgotPasswordLink = formLogin.find('.cd-form-bottom-message a'),
+		backToLoginLink = formForgotPassword.find('.cd-form-bottom-message a'),
+		mainNav = $('.main-nav');
+
+	//open modal
+	mainNav.on('click', function(event){
+		$(event.target).is(mainNav) && mainNav.children('ul').toggleClass('is-visible');
+	});
+
+	//open sign-up form
+	mainNav.on('click', '.cd-signup', signup_selected);
+	//open login-form form
+	mainNav.on('click', '.cd-signin', login_selected);
+
+	//close modal
+	formModal.on('click', function(event){
+		if( $(event.target).is(formModal) || $(event.target).is('.cd-close-form') ) {
+			formModal.removeClass('is-visible');
+		}	
+	});
+	//close modal when clicking the esc keyboard button
+	$(document).keyup(function(event){
+    	if(event.which=='27'){
+    		formModal.removeClass('is-visible');
+	    }
+    });
+
+	//switch from a tab to another
+	formModalTab.on('click', function(event) {
+		event.preventDefault();
+		( $(event.target).is( tabLogin ) ) ? login_selected() : signup_selected();
+	});
+
+	//hide or show password
+	$('.hide-password').on('click', function(){
+		var togglePass= $(this),
+			passwordField = togglePass.prev('input');
+		
+		( 'password' == passwordField.attr('type') ) ? passwordField.attr('type', 'text') : passwordField.attr('type', 'password');
+		( 'Hide' == togglePass.text() ) ? togglePass.text('Show') : togglePass.text('Hide');
+		//focus and move cursor to the end of input field
+		passwordField.putCursorAtEnd();
+	});
+
+	//show forgot-password form 
+	forgotPasswordLink.on('click', function(event){
+		event.preventDefault();
+		forgot_password_selected();
+	});
+
+	//back to login from the forgot-password form
+	backToLoginLink.on('click', function(event){
+		event.preventDefault();
+		login_selected();
+	});
+
+	function login_selected(){
+		mainNav.children('ul').removeClass('is-visible');
+		formModal.addClass('is-visible');
+		formLogin.addClass('is-selected');
+		formSignup.removeClass('is-selected');
+		formForgotPassword.removeClass('is-selected');
+		tabLogin.addClass('selected');
+		tabSignup.removeClass('selected');
+	}
+
+	function signup_selected(){
+		mainNav.children('ul').removeClass('is-visible');
+		formModal.addClass('is-visible');
+		formLogin.removeClass('is-selected');
+		formSignup.addClass('is-selected');
+		formForgotPassword.removeClass('is-selected');
+		tabLogin.removeClass('selected');
+		tabSignup.addClass('selected');
+	}
+
+	function forgot_password_selected(){
+		formLogin.removeClass('is-selected');
+		formSignup.removeClass('is-selected');
+		formForgotPassword.addClass('is-selected');
+	}
+
+	//REMOVE THIS - it's just to show error messages 
+	formLogin.find('input[type="submit"]').on('click', function(event){
+		event.preventDefault();
+		formLogin.find('input[type="email"]').toggleClass('has-error').next('span').toggleClass('is-visible');
+	});
+	formSignup.find('input[type="submit"]').on('click', function(event){
+		event.preventDefault();
+		formSignup.find('input[type="email"]').toggleClass('has-error').next('span').toggleClass('is-visible');
+	});
+
+
+	//IE9 placeholder fallback
+	//credits http://www.hagenburger.net/BLOG/HTML5-Input-Placeholder-Fix-With-jQuery.html
+	if(!Modernizr.input.placeholder){
+		$('[placeholder]').focus(function() {
+			var input = $(this);
+			if (input.val() == input.attr('placeholder')) {
+				input.val('');
+		  	}
+		}).blur(function() {
+		 	var input = $(this);
+		  	if (input.val() == '' || input.val() == input.attr('placeholder')) {
+				input.val(input.attr('placeholder'));
+		  	}
+		}).blur();
+		$('[placeholder]').parents('form').submit(function() {
+		  	$(this).find('[placeholder]').each(function() {
+				var input = $(this);
+				if (input.val() == input.attr('placeholder')) {
+			 		input.val('');
+				}
+		  	})
+		});
+	}
+    	var $lateral_menu_trigger = $('#cd-menu-trigger'),
+		$content_wrapper = $('.cd-main-content'),
+		$navigation = $('header');
+
+	//open-close lateral menu clicking on the menu icon
+	$lateral_menu_trigger.on('click', function(event){
+		event.preventDefault();
+		
+		$lateral_menu_trigger.toggleClass('is-clicked');
+		$navigation.toggleClass('lateral-menu-is-open');
+		$content_wrapper.toggleClass('lateral-menu-is-open').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+			// firefox transitions break when parent overflow is changed, so we need to wait for the end of the trasition to give the body an overflow hidden
+			$('body').toggleClass('overflow-hidden');
+		});
+		$('#cd-lateral-nav').toggleClass('lateral-menu-is-open');
+		
+		//check if transitions are not supported - i.e. in IE9
+		if($('html').hasClass('no-csstransitions')) {
+			$('body').toggleClass('overflow-hidden');
+		}
+	});
+
+	//close lateral menu clicking outside the menu itself
+	$content_wrapper.on('click', function(event){
+		if( !$(event.target).is('#cd-menu-trigger, #cd-menu-trigger span') ) {
+			$lateral_menu_trigger.removeClass('is-clicked');
+			$navigation.removeClass('lateral-menu-is-open');
+			$content_wrapper.removeClass('lateral-menu-is-open').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(){
+				$('body').removeClass('overflow-hidden');
+			});
+			$('#cd-lateral-nav').removeClass('lateral-menu-is-open');
+			//check if transitions are not supported
+			if($('html').hasClass('no-csstransitions')) {
+				$('body').removeClass('overflow-hidden');
+			}
+
+		}
+	});
+
+	//open (or close) submenu items in the lateral menu. Close all the other open submenu items.
+	$('.item-has-children').children('a').on('click', function(event){
+		event.preventDefault();
+		$(this).toggleClass('submenu-open').next('.sub-menu').slideToggle(200).end().parent('.item-has-children').siblings('.item-has-children').children('a').removeClass('submenu-open').next('.sub-menu').slideUp(200);
+	});
+
+});
+
+var d = document;
+var wrap = d.querySelector('.wrap');
+var items = d.querySelector('.items');
+var itemCount = d.querySelectorAll('.item').length;
+var scroller = d.querySelector('.scroller');
+var pos = 0;
+var transform = Modernizr.prefixed('transform');
+
+function setTransform() {
+  items.style[transform] = 'translate3d(' + (-pos * items.offsetWidth) + 'px,0,0)';
+}
+
+function prev() {
+  pos = Math.max(pos - 1, 0);
+  setTransform();
+}
+
+function next() {
+  pos = Math.min(pos + 1, itemCount - 1);
+  setTransform();
+}
+
+window.addEventListener('resize', setTransform);
+
+//credits http://css-tricks.com/snippets/jquery/move-cursor-to-end-of-textarea-or-input/
+jQuery.fn.putCursorAtEnd = function() {
+	return this.each(function() {
+    	// If this function exists...
+    	if (this.setSelectionRange) {
+      		// ... then use it (Doesn't work in IE)
+      		// Double the length because Opera is inconsistent about whether a carriage return is one character or two. Sigh.
+      		var len = $(this).val().length * 2;
+      		this.focus();
+      		this.setSelectionRange(len, len);
+    	} else {
+    		// ... otherwise replace the contents with itself
+    		// (Doesn't work in Google Chrome)
+      		$(this).val($(this).val());
+    	}
+	});
+};
+
